86ca283a1353158ae8de796657ee4525ae7be6cc
avi3175 <avishahriar@.com>
2025-02-25 22:33:12 +0600

    recheck

--- library_management_project/settings.py
+++ library_management_project/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# import os
+import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
